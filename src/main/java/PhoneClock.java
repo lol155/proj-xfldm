e76a35e0db1a1826b3eff3fa0276ceac070870a5
lol155 <1055455968@qq.com>
2016-05-12 21:49:04 +0800

    Added field PhoneClock

--- src/main/java/PhoneClock.java
+++ src/main/java/PhoneClock.java
@@ -2,6 +2,8 @@
  * Created by admin on 2016/5/12.
  */
 public class PhoneClock extends Clock {
+    private UtcTime utcTime;
+
     public void setLocalTime(int localTime) {
         this.localTime = localTime;
         this.utcTime.setUtcZeroTime(localTime - UTC_OFFSET);
