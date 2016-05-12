87b4a85c98c9855e1b1b8fcc172bc5d33b8df3c4
lol155 <1055455968@qq.com>
2016-05-12 20:49:33 +0800

    Added static method Clock.toLocalTime()

--- src/main/java/Clock.java
+++ src/main/java/Clock.java
@@ -5,6 +5,10 @@ public abstract class Clock{
     private final int UTC_OFFSET = 0;
     private int localTime = 0;
     public  abstract  void  setLocalTime (int localTime);
+
+    public static int toLocalTime(int utcZeroTime) {
+        return utcZeroTime + UTC_OFFSET;
+    }
 }
 
 
