fa76e9df6627f7224da4d15f569bf07b2bfe8307
lol155 <1055455968@qq.com>
2016-05-12 22:11:39 +0800

    Added the main() method to class HotelWorldClocksRunner and wrote the expected code there.

--- src/main/java/HotelWorldClocksRunner.java
+++ src/main/java/HotelWorldClocksRunner.java
@@ -2,5 +2,16 @@
  * Created by admin on 2016/5/12.
  */
 public class HotelWorldClocksRunner {
+    public void main(String[] args){
+        TimeSubject utcTime = new UtcTime();
+        utcTime.attach("beijing",new CityClock(8));
+        utcTime.attach("london",new CityClock(0));
+        utcTime.attach("moscow",new CityClock(4));
+        utcTime.attach("sydney",new CityClock(10));
+        utcTime.attach("newYork",new CityClock(-5));
+        Clock phoneClock = new PhoneClock(utcTime);
 
+        phoneClock.setLocalTime(9);
+        utcTime.printTimeOfAllClocks();
+    }
 }
