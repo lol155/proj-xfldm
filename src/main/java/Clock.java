bd8cccfb88156ee92e5a1436213653d0368aceb8
lol155 <1055455968@qq.com>
2016-05-12 21:45:40 +0800

    Make field Clock.localTime,UTC_OFFSET protected .

--- src/main/java/Clock.java
+++ src/main/java/Clock.java
@@ -2,8 +2,8 @@
  * Created by admin on 2016/5/8.
  */
 public abstract class Clock{
-    private final static int UTC_OFFSET = 0;
-    private int localTime = 0;
+    protected final static int UTC_OFFSET = 0;
+    protected int localTime = 0;
     public  abstract  void  setLocalTime (int localTime);
 
     public static int toLocalTime(int utcZeroTime) {
