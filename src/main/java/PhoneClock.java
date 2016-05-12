aaf6e0a1b03c3b1774b120ac962085d2938f6dc5
lol155 <1055455968@qq.com>
2016-05-12 21:36:19 +0800

    Implemented method Phone Clock.setLocalTime() according to the class diagram

--- src/main/java/PhoneClock.java
+++ src/main/java/PhoneClock.java
@@ -1,5 +1,9 @@
 /**
  * Created by admin on 2016/5/12.
  */
-public class PhoneClock {
+public class PhoneClock extends Clock {
+    public void setLocalTime(int localTime) {
+        this.localTime = localTime;
+        this.utcTime.setUtcZeroTime(localTime - UTC_OFFSET);
+    }
 }
