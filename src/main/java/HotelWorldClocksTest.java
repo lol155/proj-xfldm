c48419c169a5794ce714568dc6eaddcf2e947bb9
lol155 <1055455968@qq.com>
2016-05-13 21:30:10 +0800

    Added test the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() and wrote an asserition.

---
+++ src/main/java/HotelWorldClocksTest.java
@@ -0,0 +1,6 @@
+public class HotelWorldClocksTest{
+    @Test
+    public void the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time(){
+        assertEquals(1, londonClock.getTime());
+    }
+}
