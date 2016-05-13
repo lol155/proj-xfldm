7e7383831f9a616d214896217d82fcdf023d419b
lol155 <1055455968@qq.com>
2016-05-13 21:56:49 +0800

    Created method PhoneClock.setCityClock().

--- src/main/java/PhoneClock/PhoneClock.java
+++ src/main/java/PhoneClock/PhoneClock.java
@@ -1,10 +1,16 @@
 package PhoneClock;
 
+import CityClock.CityClock;
+
 /**
  * Created by admin on 2016/5/13.
  */
 public class PhoneClock {
     public PhoneClock(int utcOffset) {
+
+    }
+
+    public void setCityClock(CityClock.CityClock cityClock) {
 
     }
 }
