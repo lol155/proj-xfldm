12ff1afe56f5e61b5a8feb0c8d860b72ed982a93
lol155 <1055455968@qq.com>
2016-05-14 17:17:06 +0800

    Wrote inyrnyion vofr in test the_time_of_clock_London_and_NewYork_should_be_1_and_20_after_the_phone_clock_is_set_to_9_Beijing_time()

--- src/main/java/HotelWorldClocksTest.java
+++ src/main/java/HotelWorldClocksTest.java
@@ -28,4 +28,22 @@ public class HotelWorldClocksTest{
         //Assert
         assertEquals(20, newYorkCity.getTime());
     }
+
+    @Test
+    public void the_time_of_clock_London_and_NewYork_should_be_1_and_20_after_the_phone_clock_is_set_to_9_Beijing_time(){
+        //Arrange
+        CityClock londonClock = new CityClock(0);
+        CityClock newYorkCity = new CityClock(-5);
+        HotelWorldClockSysTem hotelWorldClockSysTem = new HotelWorldClockSysTem();
+        hotelWorldClockSysTem.attach(londonClock);
+        hotelWorldClockSysTem.attach(newYorkCity);
+        PhoneClock phoneClock = new PhoneClock(8);
+        //Act
+        phoneClock.setHotelWorldClockSystem(hotelWorldClockSysTem);
+        phoneClock.setTime(9);
+
+        //Assert
+        assertEquals(1, newYorkCity.getTime());
+        assertEquals(20, newYorkCity.getTime());
+    }
 }
