6485625f5b9295c72ecd731c22a7a7c085f2e994
lol155 <1055455968@qq.com>
2016-05-14 14:12:45 +0800

    Created method CityClock.setUtcZeroTime() and assigned the UTC zero time to the field of class CityClock.

--- src/main/java/PhoneClock.java
+++ src/main/java/PhoneClock.java
@@ -3,9 +3,10 @@
  */
 public class PhoneClock {
     private CityClock cityClock;
+    private int utcOffset
 
     public PhoneClock(int utcOffset) {
-
+        this.utcOffset = utcOffset;
     }
 
     public void setCityClock(CityClock cityClock) {
