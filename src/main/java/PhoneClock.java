4852eede625a4cd2fc4759ba561b36edc5c23dbc
lol155 <1055455968@qq.com>
2016-05-14 14:10:05 +0800

    Created field cityClock and assigned a value to it in class PhoneClock.

--- src/main/java/PhoneClock.java
+++ src/main/java/PhoneClock.java
@@ -2,15 +2,19 @@
  * Created by admin on 2016/5/13.
  */
 public class PhoneClock {
+    private CityClock cityClock;
+
     public PhoneClock(int utcOffset) {
 
     }
 
-    public void setCityClock(CityClock londonClock) {
+    public void setCityClock(CityClock cityClock) {
+        this.cityClock = cityClock;
     }
 
 
     public void setTime(int time) {
         this.cityClock.setUtcZeroTime(time - this.utcOffset);
     }
+
 }
